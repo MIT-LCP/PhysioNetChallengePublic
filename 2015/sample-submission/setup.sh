7dc39dfcc1105b369bc1a9b0b4936f302e1bcfa9
ikarosilva <ikaro@mit.edu>
2015-02-11 16:40:44 -0500

    added variable defining MATLAB entries

--- 2015/sample-submission/setup.sh
+++ 2015/sample-submission/setup.sh
@@ -6,3 +6,5 @@
 # This bash script performs any setup necessary in order to test your entry.
 # It is run only once, before running any other code belonging to your entry.
 
+#Remove (or set it to 0) if you are not using MATLAB
+NEED_MATLAB=1
